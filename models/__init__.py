eb6312a8fe02e478f5c4faca87ca3a8b8f7c7c49
alontalmor <salont@gmail.com>
2019-07-07 11:23:34 +0300

    support official hotpotqa eval script

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
 from models.multiqa_reader import *
-from models.mrqa_predictor import *
+from models.multiqa_predictor import *
 from models.multiqa_bert import *
 from models.multiqa_iterator import *
