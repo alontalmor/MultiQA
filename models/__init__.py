010a1057c04c0b54f2762edfcac4f09a9c602bff
alontalmor <salont@gmail.com>
2019-07-18 18:58:39 +0300

    old version testing / debugging

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
+from models.multiqa_reader_old import *
 from models.multiqa_reader import *
 from models.multiqa_predictor import *
 from models.multiqa_bert import *
+from models.multiqa_bert_old import *
 from models.multiqa_iterator import *
