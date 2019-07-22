0e391df75e76c40259161e7324a98ad288feb9e7
alontalmor <salont@gmail.com>
2019-07-22 12:08:10 +0300

    testing old version

--- models/multiqa_predictor.py
+++ models/multiqa_predictor.py
@@ -18,20 +18,26 @@ class MultiQAPredictor(Predictor):
             for instance in self._dataset_reader.gen_question_instances(question_chunks):
                 question_instances.append(instance)
 
-            question_predictions = self.predict_batch_instance(question_instances)
+            if len(question_instances) > 0:
+                question_predictions = self.predict_batch_instance(question_instances)
 
-            max_logit = -1000
-            final_question_pred = {}
-            for pred in question_predictions:
-                if pred['best_span_logit'] + pred['yesno_logit'] > max_logit:
-                    max_logit = pred['best_span_logit'] + pred['yesno_logit']
-                    final_question_pred['best_span_str'] = pred['best_span_str']
+                max_logit = -1000
+                final_question_pred = {}
+                for pred in question_predictions:
+                    if pred['best_span_logit'] + pred['yesno_logit'] > max_logit:
+                        max_logit = pred['best_span_logit'] + pred['yesno_logit']
+                        final_question_pred['best_span_str'] = pred['best_span_str']
 
-            # Leaving only the original question ID for this dataset in order to run the original eval script.
-            final_question_pred['qid'] = pred['qid']
-            if pred['qid'].find('_q_') > -1:
-                final_question_pred['qid'] = final_question_pred['qid'].split('_q_')[1]
-            predictions.append(final_question_pred)
+                # Leaving only the original question ID for this dataset in order to run the original eval script.
+                final_question_pred['qid'] = pred['qid']
+                if pred['qid'].find('_q_') > -1:
+                    final_question_pred['qid'] = final_question_pred['qid'].split('_q_')[1]
+                predictions.append(final_question_pred)
+            else:
+                qid = example['qas'][0]['qid']
+                if qid.find('_q_') > -1:
+                    qid = qid.split('_q_')[1]
+                predictions.append({'qid':qid ,'best_span_str':''})
 
         formated_predictions = {pred['qid']:pred['best_span_str'] for pred in predictions}
         return formated_predictions
